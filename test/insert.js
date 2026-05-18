7dbdfdd8eac9a3c1c334bc8dadb788499b59549b
keshavsoft <nknnkumar@yahoo.com>
2026-05-18 08:42:31 +0530

    need to test both

---
+++ test/insert.js
@@ -0,0 +1,18 @@
+import getLatestVersion from "../bin/core/getLatestVersion.js";
+const commandToSend = "Insert";
+
+const load = async (cmd) => {
+    const v = getLatestVersion();
+    return (await import(`../bin/${v}/commands/exportCommands/${cmd}.js`)).default;
+};
+
+const startFunc = async () => {
+    const func = await load(commandToSend);
+
+    func({
+        cmd: commandToSend,
+        toPath: process.cwd()
+    });
+};
+
+startFunc().then();
